9d81b96b81c3caee6ccaea749b54b585750b13fa
Constantine Chutis <constantinechutis@gmail.com>
2019-02-06 23:50:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ function whisper(string) {
 function logShout(string) {
   return console.log(string.toUpperCase());
 }
+
+function logWhisper(string) {
+  return console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return console.log("I can't hear you!");
+  } else if (string.toUpperCase() === string) {
+    return console.log("YES INDEED!");
+  }
+}
